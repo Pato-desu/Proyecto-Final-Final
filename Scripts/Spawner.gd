94a58350e82ea1943c663097f447864891ae0312
Patricio <patriciovercesi@hotmail.com>
2020-10-22 23:23:00 -0300

    el true spawner

--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -1,15 +1,25 @@
-extends Timer
+extends Node
 
-const enemy1 = preload("res://Scenes/Enemies/1A.tscn")
-const enemy2 = preload("res://Scenes/Enemies/1B.tscn")
+export var z0 = preload("res://Scenes/Enemies/1A.tscn")
+export var z1 = preload("res://Scenes/Enemies/1B.tscn")
+var z = [z0, z1]
+
+export var Enemies = PoolVector3Array()
+var enemies = [Vector3(1, 1, 1), Vector3(0, 2, 1)]
 var aux
+var timer = 0.0
 
-func _on_Spawner_timeout():
-	randomize()
-	if randi() % 2:
-		aux = enemy1.instance()
-	else:
-		aux = enemy2.instance()
-	randomize()
-	aux.init(2000, rand_range(50, 1000))
+func _ready():
+	enemies = Enemies
+
+func _process(delta):
+	timer = timer + delta
+	if enemies:
+		if enemies[0].x < timer:
+			spawn(enemies[0])
+			enemies.remove(0)
+			
+func spawn(enemy):
+	aux = z[enemy.z].instance()
+	aux.init(2000, enemy.y)
 	add_child(aux)
