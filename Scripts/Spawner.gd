b7a8935f33a7285be21bc7d56ff7fcf8254fcac3
Patricio <patriciovercesi@hotmail.com>
2020-11-21 03:44:31 -0300

    Todos los sprites de enemigos y programacion del tercero

--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -2,15 +2,16 @@ extends Node
 
 export var z0 = preload("res://Scenes/Enemies/1A.tscn")
 export var z1 = preload("res://Scenes/Enemies/1B.tscn")
-var z = [z0, z1]
+export var z2 = preload("res://Scenes/Enemies/1C.tscn")
+var z = [z0, z1, z2]
 
-export var Enemies = PoolVector3Array()
-var enemies = [Vector3(1, 1, 1), Vector3(0, 2, 1)]
+export var enemies = PoolVector3Array()
+#var enemies = [Vector3(1, 1, 1), Vector3(0, 2, 1)]
 var aux
 var timer = 0.0
-
-func _ready():
-	enemies = Enemies
+#
+#func _ready():
+#	enemies = Enemies
 
 func _process(delta):
 	timer = timer + delta
