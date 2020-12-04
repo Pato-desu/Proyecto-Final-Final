c8e1ce1f737f63a6a51325d1dc5a7d2badd08941
Patricio <patriciovercesi@hotmail.com>
2020-12-04 05:24:58 -0300

    La data del nivel y los enemigos es leido desde hoja de calc

--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -1,26 +1,51 @@
 extends Node
 
+var table = "res://Data/ExN.prn"
+
 export var z0 = preload("res://Scenes/Enemies/1A.tscn")
 export var z1 = preload("res://Scenes/Enemies/1B.tscn")
 export var z2 = preload("res://Scenes/Enemies/1C.tscn")
 var z = [z0, z1, z2]
 
-export var enemies = PoolVector3Array()
-#var enemies = [Vector3(1, 1, 1), Vector3(0, 2, 1)]
+var Enemies = PoolVector3Array()
 var aux
 var timer = 0.0
-#
-#func _ready():
-#	enemies = Enemies
+var dic
+
+func _ready():
+	dic = csv2Dict()
+	for i in dic["Index"].size() - 2:
+		aux = Vector3(dic["Time"][i], dic["Height"][i], dic["Type"][i])
+		Enemies.append(aux)
 
 func _process(delta):
 	timer = timer + delta
-	if enemies:
-		if enemies[0].x < timer:
-			spawn(enemies[0])
-			enemies.remove(0)
+	if Enemies:
+		if Enemies[0].x < timer:
+			spawn(Enemies[0])
+			Enemies.remove(0)
+			
 			
 func spawn(enemy):
 	aux = z[enemy.z].instance()
 	aux.init(2000, enemy.y)
 	add_child(aux)
+
+
+func csv2Dict():
+	var file = File.new()
+	assert(file.file_exists(table))
+	file.open(table, File.READ)
+	var content = file.get_as_text()
+	content = content.split("\n")
+	content = Array(content)
+	content.erase("")
+	var dict = {}
+	for item in content:
+		var items = item.split(",")
+		items = Array(items)
+		items.erase("")
+		dict[items[0]] = items.duplicate()
+		dict[items[0]].erase(items[0])
+	file.close()
+	return dict
