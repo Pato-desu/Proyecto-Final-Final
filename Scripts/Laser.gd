bdec1bd531468d6faa55c5fcbbf91cb170af657b
Patricio <patriciovercesi@hotmail.com>
2020-12-16 03:18:05 -0300

    no estoy seguro q cambie, no me acuerdo

--- Scripts/Laser.gd
+++ Scripts/Laser.gd
@@ -5,14 +5,13 @@ onready var dagame = game.get_node("Damage")
 const damage = 1.8 #100 de daño por segundo aprox
 onready var visual = $Visual
 
-func _process(_delta):
+func _pprocess(_delta):
 	visual.points[1] = get_collision_point() - global_position
 	for i in visual.get_point_count() - 2:
 		visual.remove_point(2) #resize nunca me anduvo para nada
-	if is_colliding():
-		var collider = get_collider()
-		if collider.is_in_group("Elastic"):
-			bounce(cast_to, get_collision_point(), get_collision_normal(), collider, 3)
+	var collider = get_collider()
+	if collider.is_in_group("Elastic"):
+		bounce(cast_to, get_collision_point(), get_collision_normal(), collider, 3)
 
 func activate():
 	modulate = Color(1, 1, 1, 1)
@@ -33,22 +32,21 @@ func bounce(prev, pt, norm, col, n):
 	raycast.cast_to = prev.bounce(norm)
 	raycast.collision_mask = 94
 	raycast.enabled = true
-#	raycast.exclude_parent = false
+	raycast.exclude_parent = false
 	raycast.collide_with_bodies = true
 	raycast.collide_with_areas = true
 	if col.has_method("bounce_l"):
 		col.bounce_l(raycast)
-	var new_pt
 	raycast.force_raycast_update()
+	var new_pt
+	print(raycast.get_collider())
 	if raycast.is_colliding():
-#			var Col = raycast.get_collider()
-		print("EY")
+#		print("EY")
 		new_pt = raycast.get_collision_point() - raycast.global_position
-		#RECURSIVIDAD SI TMB ES ELASTIC BOMBASTIC. SOLO DESP DE "IMPACTO"
+		#RECURSIVIDAD SI TMB ES ELASTIC
 	else:
+#		print(raycast.position)
 		new_pt = raycast.cast_to
-#		print(raycast.collision_mask)
 	visual.add_point(new_pt)
 	remove_child(raycast)
 	raycast.free()
-	#IMPACTO
