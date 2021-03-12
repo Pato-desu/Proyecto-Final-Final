b017924e51d7f15663b06dcd2449bbea6a0c8a59
Patricio <patriciovercesi@hotmail.com>
2021-03-12 19:17:12 -0300

    idk bro

--- Scripts/UI/Menus/GameOver.gd
+++ Scripts/UI/Menus/GameOver.gd
@@ -2,10 +2,11 @@ extends Popup
 
 func _process(_delta):
 	if visible:
+		 #se deberia poder salir con ESC
 		if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("reset"):
 			get_tree().paused = false
 # warning-ignore:return_value_discarded
-			get_tree().reload_current_scene() #BUGS CON ESCAPE Y CLICK IZQ?
+			get_tree().reload_current_scene()
 #		if Input.is_action_just_pressed("escape"):
 #			get_tree().paused = false
 #			get_tree().reload_current_scene()
@@ -15,7 +16,7 @@ func pop_up(win):
 	get_tree().paused = true
 	popup_centered()
 	if win:
-		get_node("Message").text = "You Won!"
+		get_node("VBoxContainer/Message").text = "You Won!"
 	else:
-		get_node("Message").text = "You Lost!"
+		get_node("VBoxContainer/Message").text = "You Lost!"
 	
--- Scripts/UI/Menus/SceneManagement.gd
+++ Scripts/UI/Menus/SceneManagement.gd
@@ -1,9 +1,13 @@
-extends VBoxContainer
+extends Node
 
 func button_pressed(name):
 	match name:
 		"play":
 # warning-ignore:return_value_discarded
 			get_tree().change_scene("res://Scenes/Main/Game.tscn")
+		"menu":
+			get_tree().change_scene("res://Scenes/Main/Menu.tscn")
+		"resume":
+			get_tree().paused = false
 		"exit":
 			get_tree().quit()
