5acbfacddce40fc4e6b46467a63c1256d3cbb904
Patricio <patriciovercesi@hotmail.com>
2021-04-09 20:33:05 -0300

    pijaaaaa

--- Scripts/General/Laser.gd
+++ Scripts/General/Laser.gd
@@ -1,13 +1,12 @@
 extends RayCast2D
 
-const sparks = preload("res://sparks.tscn")
 onready var game = get_node("/root/Game")
 onready var damager = game.get_node("Damager")
 onready var player = game.get_node("Player/Hurtbox")
 onready var line = $Line
 onready var glow = $Glow
+onready var sparks = $Sparks
 const damage = 1.8 #100 D daño por segundo aprox
-onready var line = $line
 var activated = false
 const max_reflects = 6
 var space_state
@@ -20,6 +19,7 @@ func _physics_process(_delta):
 	#Limpiado
 	for i in line.get_point_count() - 2:
 		line.remove_point(2) #Resize nunca me anduvo para nada
+	sparks.emitting = false
 	#Comportamiento recursivo en caso de rebote:
 	behaviour(cast_to, global_position, player, 0)
 	
@@ -41,19 +41,23 @@ func behaviour(cast, from, prev, n):
 				to = collider.laser_bounce(to, from)
 			if n <= max_reflects:
 				behaviour(to, result.position, collider, n+1) #Recursividad
-		#Activacion del sensor
-		elif collider.get_collision_layer_bit(8):
-			hover(collider)
-			if activated:
-				if collider.has_method("laser_sensor"):
-					collider.laser_sensor(self, result.normal.normalized())
-#				else:
-#					print("Está mal la máscara o la función")
-		#Dañado
 		else:
-			hover(collider)
-			if activated:
-				damager.damage(damager.find_dmged(collider), damage) #Si es algo con vida lo daña
+			sparks.position = result.position - global_position
+			#Activacion del sensor
+			if collider.get_collision_layer_bit(8):
+				hover(collider)
+				if activated:
+					sparks.emitting = true
+					if collider.has_method("laser_sensor"):
+						collider.laser_sensor(self, result.normal.normalized())
+	#				else:
+	#					print("Está mal la máscara o la función")
+			#Dañado
+			else:
+				hover(collider)
+				if activated:
+					sparks.emitting = true
+					damager.damage(damager.find_dmged(collider), damage) #Si es algo con vida lo daña
 
 func hover(col):
 	if col.has_node("Sprite") and col.get_node("Sprite").has_method("select"):
