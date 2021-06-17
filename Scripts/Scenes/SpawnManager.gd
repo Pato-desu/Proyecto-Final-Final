c09b842c6853627f747e65fe34ec9c8d425cf8e1
Patricio <patriciovercesi@hotmail.com>
2021-06-17 17:35:14 -0300

    xd

--- Scripts/Scenes/SpawnManager.gd
+++ Scripts/Scenes/SpawnManager.gd
@@ -6,31 +6,36 @@ const default_x = 2000
 var i = 0
 var aux
 var timer = 0.0
+var wait = 1
 var list
 
 func _ready():
 	assert(game.maps.size() > game.selected_map)
 	list = csv_to_dictionary(game.maps[game.selected_map])
+	#UFF EL QUILOMBO DE PRIMER SPAWN
+#	spawn()
 #	else:
 #		print("bad selection")
 
 func _process(delta):
 	timer = timer + delta
-	if i < list["Index"].size() and float(list["Time"][i]) < timer:
+	if wait < timer:
 		spawn()
+		timer = 0.0
+		if i < list["Index"].size():
+			wait = float(list["Time"][i])
+		else:
+			game.over(true)
+		i += 1
 			
 func spawn():
 	aux = game.enemies[int(list["Type"][i])].instance()
 	if bool(int(list["Height"][i])):
 		aux.init(default_x, float(list["Height"][i]))
 	add_child(aux)
-	i += 1
 
 func next():
-	if i < list["Index"].size():
-		spawn()
-	else:
-		game.over(true)
+	wait = 0
 
 func csv_to_dictionary(map):
 	var file = File.new()
@@ -38,6 +43,8 @@ func csv_to_dictionary(map):
 	file.open(map, File.READ)
 	var content = file.get_as_text()
 	content = content.split("\n")
+	wait = float(content[0])
+	content.remove(0)
 	content = Array(content)
 	content.erase("")
 	var dict = {}
